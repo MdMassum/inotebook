dc4fc530ddf72787e0f676250737c15edceda06d
Md Emamudin <mdemamudin726@gmail.com>
2024-05-23 15:27:46 +0530

    Connected-MongoAtlas

--- Backend/db.js
+++ Backend/db.js
@@ -1,9 +1,17 @@
 const mongoose = require('mongoose');
-const mongoUrl = "mongodb://localhost:27017/inotebook"
+
+// for using env file instal dotenv
+// Install dotenv: npm install dotenv
+
+
+// const mongoUrl = process.env.REACT_APP_MONGO_URL;
+const mongoUrl = "mongodb+srv://mdemam:Masum2002@cluster0.p2oztto.mongodb.net/iNotebook?retryWrites=true&w=majority&appName=Cluster0"
+// console.log(mongoUrl);
 
 async function ConnectToMongo() {
     await mongoose.connect(mongoUrl)
     .then(()=>console.log("Connected to Mongo Successfully"))
     .catch(err => console.log(err));
   }
+
 module.exports = ConnectToMongo;
--- Backend/index.js
+++ Backend/index.js
@@ -3,11 +3,11 @@ const cors = require('cors')
 // const bodyParser = require('body-parser')
 const express = require('express')
 
-ConnectToMongo();
 
 const app = express()
 const port = 5000
 
+ConnectToMongo();
 // app.use(bodyParser.json()) // use this or below express.json() to use req.body
 app.use(express.json())  // we use this to use req.body for json file i.e now we can recieve and send json by req.body (see also body-parser was used earlier when express.json() was not there we can also use bodyParser.json() )
 app.use(cors()); // for solving issue while connecting frontend and backend
